36bc7c2107d8122e21feecae2030222d19a18433
Amit Kumar <15000amitkumar@gmail.com>
2024-05-07 01:35:06 +0530

    Auth Middleware

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
+
+# Hono # 
+Hono - [炎] means flame🔥 in Japanese - is a small, simple, and ultrafast web framework for the Edges. It works on any JavaScript runtime: Cloudflare Workers, Fastly Compute, Deno, Bun, Vercel, Netlify, AWS Lambda, Lambda@Edge, and Node.js.
+
+
+
+To start the hono project
+
+
+```
+npm create hono@latest
+```
+
+
+
 ```
 npm install
 npm run dev
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-import { Hono } from "hono";
+import { Hono, Next } from "hono";
+import { Context } from "hono/jsx";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+async function authMiddleWare(c: any, next: any) {
+  if (c.req.header("Authorization")) {
+    await next();
+  } else {
+    return c.text("You don't have access");
+  }
+}
+
+app.get("/", authMiddleWare, async (c) => {
+  const body = await c.req.parseBody();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.json({ msg: "You got access" });
 });
 
 export default app;
